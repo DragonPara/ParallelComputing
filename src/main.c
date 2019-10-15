31966c98a7739c7ce436f0797a79853f31b8f8f6
dragon <715077252@qq.com>
2019-10-15 21:25:26 +0800

    fix

--- src/main.c
+++ src/main.c
@@ -5,13 +5,13 @@ int main(int argc, char *argv[])
     int time1=clock(),time2;
     int tmp=0;
 #pragma omp parallel for
-    for (int i = 0; i < 100000; i++)
+    for (int i = 0; i < 10; i++)
     {
-        tmp+=i;
-        if(i%1000==0)
+//        tmp+=i;
+//        if(i%10==0)
         printf("%d\n",i);
     }
-    printf("i = %d\n", tmp);
+//    printf("i = %d\n", tmp);
     time2=clock();
     printf("%d",time2-time1);
     return 0;
