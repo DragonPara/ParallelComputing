d7cfc3d283a10c1be68208b04be3f5ba84c3b2fb
dragon <715077252@qq.com>
2019-10-16 08:41:12 +0800

    primeNumber

--- src/main.c
+++ src/main.c
@@ -1,14 +1,20 @@
 #include<time.h>
 #include <stdio.h>
+#include<math.h>
 int main(int argc, char *argv[])
 {
     int time1=clock(),time2;
     int tmp=0;
-#pragma omp parallel for
-    for (int i = 0; i < 10; i++)
+    #pragma omp parallel for
+    for (int i = 0; i < 10000; i++)
     {
 //        tmp+=i;
 //        if(i%10==0)
+        int count=2;
+        for(;count<=pow(i,0.5);count++)
+            if(i%count==0)
+                break;
+        if(count>pow(i,0.5))
         printf("%d\n",i);
     }
 //    printf("i = %d\n", tmp);
