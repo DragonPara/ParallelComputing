0800bc09a92c7228282bf38a17ac17a83e374f7d
dragon <715077252@qq.com>
2020-01-20 16:32:23 +0800

    test

---
+++ src/Message Passing Interface/Rank&Size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<mpi.h>
+int main(int argc, char **argv){
+	int rank,size;
+	MPI_Init(*argc,*argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+	printf("The %d processor of %d\n",rank,size);
+}
