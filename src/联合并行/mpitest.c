872d765710399bf9e593819db0333dadd793aeab
dragon20180618 <715077252@qq.com>
2020-07-30 18:45:25 +0800

    mpi+cuda+openmp

---
+++ src/联合并行/mpitest.c
@@ -0,0 +1,14 @@
+#include<mpi.h>
+#include<omp.h>
+#include<stdio.h>
+int main(){
+    MPI_Init( NULL , NULL);
+    int rank,size;
+    MPI_Comm_rank( MPI_COMM_WORLD , &rank);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    #pragma omp parallel num_threads(2)
+    {
+        cudaGet(omp_get_thread_num,rank);
+    }
+    MPI_Finalize();
+}
