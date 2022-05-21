ccd8435bf63951dfef2d6111256e83b1bf9f0d42
Yanlong Li <44524468+DragonPara@users.noreply.github.com>
2022-05-21 10:11:46 +0800

    Delete src/联合并行 directory

--- src/联合并行/k.cu
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-__global__ void kernel1(){
-    printf("Hi Dragon\n");
-}
-extern "C" void cudaGet(int ompNumber,int mpiNumber){
-    cudaSetDevice(ompNumber+mpiNumber);
-    kernel1<<<2,2>>>();
-    cudaDeviceSynchronize();
-}
